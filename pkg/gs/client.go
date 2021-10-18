e0b6cbb3e98bc51e8e49bbdcfc523dfa1540c6bf
Jannchie <jannchie@gmail.com>
2021-10-18 18:32:33 +0900

    feat: add send task api

--- pkg/gs/client.go
+++ pkg/gs/client.go
@@ -1,9 +1,30 @@
 package gs
 
 import (
+	"context"
 	"github.com/jannchie/gazer-system/api"
 	"google.golang.org/grpc"
 )
+
+type Client struct {
+	api.GazerSystemClient
+}
+
+func (c *Client) SendOneTask(task *api.Task) error {
+	_, err := c.AddTasks(context.Background(), &api.AddTasksReq{Tasks: []*api.Task{task}})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) SendTasks(tasks []*api.Task) error {
+	_, err := c.AddTasks(context.Background(), &api.AddTasksReq{Tasks: tasks})
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func NewClient(server string) api.GazerSystemClient {
 	opts := []grpc.DialOption{
@@ -13,5 +34,5 @@ func NewClient(server string) api.GazerSystemClient {
 	if err != nil {
 		panic(err)
 	}
-	return api.NewGazerSystemClient(conn)
+	return &Client{GazerSystemClient: api.NewGazerSystemClient(conn)}
 }
