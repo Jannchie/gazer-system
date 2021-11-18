bd50263cfd87f800edadf98a43a15355124b12cb
Jannchie <jannchie@gmail.com>
2021-11-18 15:28:22 +0900

    feat: set log flags

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"log"
+
 	"github.com/jannchie/gazer-system/internal/gs"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	gss := gs.NewDefaultServer()
 	gss.Run()
 }
